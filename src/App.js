1a05cd30e1010b3ea15fad7a25b418f2e13dea19
Sharun E Rajeev <sharunpublic@gmail.com>
2022-08-08 13:47:07 +0530

    Update root files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import CSS
 import './App.css';
+
+// Import pages
+import Home from './pages/Home'
+import Publish from './pages/Publish';
+import Users from './pages/Users';
+import Songs from './pages/Songs';
+import Profile from './pages/Profile';
+import Logout from './pages/Logout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/publish" element={ <Publish />} />
+          <Route path="/users" element={ <Users /> } />
+          <Route path="/songs" element={<Songs />} /> 
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/logout" element={ <Logout />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Add CSS imports here
+// Add all the custom CSS below
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
